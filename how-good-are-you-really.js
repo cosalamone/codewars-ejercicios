aa6e026aee24c0d210b2fed2c43c4f9ad9a525f1
cosalamone <cosalamone@gmail.com>
2022-03-27 11:03:50 -0300

    function How good are you really?

---
+++ how-good-are-you-really.js
@@ -0,0 +1,26 @@
+/* There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the 
+average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average 
+and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. For 
+calculating the average point you may add your point to the given array!*/
+
+function betterThanAverage(classPoints, yourPoint) {
+    let totalPoints = sumaDeNotas(classPoints) + yourPoint
+    let Average = totalPoints / (classPoints.length + 1) 
+    return yourPoint > Average
+}
+
+
+function sumaDeNotas(notas) {
+    var total = 0
+    for (i = 0; i < notas.length; i = i + 1) {
+        total = total + notas[i]
+    } return total
+}
