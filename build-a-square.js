4a16bfb43d003fd52e94cfc850145dedcdcbd389
cosalamone <cosalamone@gmail.com>
2022-03-27 11:19:03 -0300

    function build a square

---
+++ build-a-square.js
@@ -0,0 +1,28 @@
+/* I will give you an integer. Give me back a shape that is as long and wide 
+as the integer. The integer will be a whole number between 1 and 50.
+
+Example: n = 3, so I expect a 3x3 square back just like below as a string:
+
++++
++++
++++
+
+*/
+
+function generateShape(integer) {
+    let square = ""
+    for (let i = 0; i < integer; i = i + 1) {
+        if (i < integer - 1) {
+            square = square + hileraDeMases(integer) + "\n"
+        }
+    }
+    return square = square + hileraDeMases(integer)
+}
+
+function hileraDeMases(n) {
+    let mases = ""
+    for (let i = 0; i < n; i = i + 1) {
+        mases = mases + "+"
+    }
+    return mases
+}
